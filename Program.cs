6324f7b2ef23ab777a23e3c8b720b122170f4d90
roma1909 <rahma.abulfatouh1@gmail.com>
2025-02-09 14:15:43 +0200

    implicitly typed local variable

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            
+
+            #region implicitly typed local variable
+
+            #region var
+            //var: is a c# keyword  
+
+            var name = "rahma";
+            //must be intilized
+            //compiler can detect the variable datatype based on intilial value in the compliation time
+            //can't be intilized with null
+            //after intilization u can not change the datatype
+
+
+            //name = 9;//invalid 
+
+            #endregion
+
+            #region dynamic
+            //dynamic :is a c# keyword c# 4.0feature
+
+            dynamic data;
+            //compiler will skip the type checking at compilation time
+            //clr will resolve type at the run time
+            //you don't need to intilize it
+            // after intilization you can change the var dt
+            //
+
+
+            #endregion
+
+
+            #endregion
         }
     }
 }
